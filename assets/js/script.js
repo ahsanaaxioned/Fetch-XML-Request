73a31d99e82b3f59456f1878c7d556641d0bb600
ahsanaaxioned <ahsana@axioned.com>
2022-10-17 14:49:15 +0530

    js file added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,40 @@
-/* Author: 
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// global variable declaration start here
+const fetchList = document.querySelector(".fetch-list"),
+    btn = document.querySelector(".btn");
+let counter = 10,
+    i = 0;
+// global variable declaration end here
+// fetch function start here
+fetchList.innerText = "";
+function getData() {
+    const httpRequest = new XMLHttpRequest();
+    httpRequest.open('Get', 'https://jsonplaceholder.typicode.com/posts');
+    httpRequest.onload = function () {
+        data = JSON.parse(this.response)
+        setData(data);
+        fetchCard();
+    }
+    httpRequest.send();
+};
+getData();
+// fetch function end here
+// btn event start here
+function setData(data) {
+    btn.addEventListener("click", fetchCard);
+}
+function fetchCard() {
+    for (i; i < counter; i++) {
+        if (i == 99) {
+            btn.classList.add("btn-hide");
+        }
+        let li = document.createElement("li");
+        li.classList.add("fetch-item");
+        li.innerHTML = `<span class="heading">${data[i].id}</span>
+        <h3 class="title">${data[i].title}</h3>
+        <p class="discription">${data[i].body}</p>`
+        fetchList.appendChild(li);
+    }
+    i = counter;
+    counter += 10;
+};
+// btn event start here
